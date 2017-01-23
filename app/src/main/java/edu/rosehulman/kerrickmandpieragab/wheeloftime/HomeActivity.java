5e046c31e168f37eb2446a3223b083bce025d17d
Katrina <katrina.kerrick@gmail.com>
2017-01-22 20:00:33 -0500

    Changed tabs to the menu instead of online tutorial

--- app/src/main/java/edu/rosehulman/kerrickmandpieragab/wheeloftime/HomeActivity.java
+++ app/src/main/java/edu/rosehulman/kerrickmandpieragab/wheeloftime/HomeActivity.java
@@ -9,6 +9,7 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -20,34 +21,38 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("");
         setSupportActionBar(toolbar);
 
 
-        ViewPager vp = (ViewPager)findViewById(R.id.pager);
-        vp.setAdapter(new SampleFragmentPagerAdapter(getSupportFragmentManager(), this));
-
-        TabLayout tl = (TabLayout)findViewById(R.id.sliding_tabs);
-        tl.setupWithViewPager(vp);
+//        ViewPager vp = (ViewPager)findViewById(R.id.pager);
+//        vp.setAdapter(new SampleFragmentPagerAdapter(getSupportFragmentManager(), this));
+//
+//        TabLayout tl = (TabLayout)findViewById(R.id.sliding_tabs);
+//        tl.setupWithViewPager(vp);
 
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_home, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        switch (item.getItemId()) {
+            case R.id.action_home:
+                Log.d("TTT", "Clicked action_home");
+                return true;
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            case R.id.action_search:
+                Log.d("TTT", "Clicked action_search");
+                return true;
+
+            case R.id.action_favorites:
+                Log.d("TTT", "Clicked action_favorites");
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
